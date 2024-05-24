8a45477b4b755e6b3ba315f91628e68c58fd03f2
Ângelo Morais <angelolmorais@gmail.com>
2024-05-24 12:51:53 -0300

    database

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '172.22.1.11'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'sigoei_prod'),
             'username' => env('DB_USERNAME', 'postgres'),
